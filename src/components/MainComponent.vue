1457122e4d30bf54a82be19ab3009f0eaf56cbad
alessandro-barate <alebarat@gmail.com>
2024-06-05 22:09:38 +0100

    General scaffolding

---
+++ src/components/MainComponent.vue
@@ -0,0 +1,16 @@
+<script>
+import { store } from '../store.js';
+export default {
+    name: 'MainComponent',
+    components: {
+    },
+
+    data(){
+        return {
+            store,
+        };
+    },
+
+    methods: {},
+};
+</script>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+
+// Import style
+import './assets/scss/style.scss';
+
+// Import Vue root component
+import App from './App.vue'
+
+createApp(App).mount('#app')
